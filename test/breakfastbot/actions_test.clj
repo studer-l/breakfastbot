d1068c8ae233554d54e333da5b3c4b2362e44d4b
studer-l <lukas.studer@alumni.ethz.ch>
2019-04-05 23:54:03 +0200

    Strip white space in trigger sequence

--- test/breakfastbot/actions_test.clj
+++ test/breakfastbot/actions_test.clj
@@ -10,7 +10,7 @@
 (def non-handler
   "Never matches anything"
   {:matcher (fn [author message] nil)
-   :action (fn [[author message]] [:greedy-handler author message])
+   :action (fn [[author message]] [:non-handler author message])
    :help nil})
 
 (t/deftest try-handler
@@ -19,3 +19,17 @@
              (sut/try-handler greedy-handler "a@b.c" "foobar"))))
   (t/testing "returning nil in matcher does not trigger action"
     (t/is (nil? (sut/try-handler non-handler "a@b.c" "foobar")))))
+
+(t/deftest dispatch-handlers
+  (t/testing "ignores non-trigger inputs"
+    (t/is (nil? (sut/dispatch-handlers [non-handler greedy-handler]
+                                       "Flume" "Jewel"))))
+  (t/testing "matches trigger sequence"
+    (t/is (= [:greedy-handler "a@b.c" "quux"]
+             (sut/dispatch-handlers [non-handler greedy-handler]
+                                    "a@b.c" "@**Breakfast Bot** quux"))))
+
+  (t/testing "matches trigger sequence removing space"
+    (t/is (= [:greedy-handler "a@b.c" "baz"]
+             (sut/dispatch-handlers [non-handler greedy-handler]
+                                    "a@b.c" "@**Breakfast Bot**    baz")))))
