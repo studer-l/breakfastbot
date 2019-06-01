8781dcbc5b31ab91aa2bd3878294cabf237c8a9a
studer-l <lukas.studer@alumni.ethz.ch>
2019-06-01 20:11:44 +0200

    Fixes test

--- src/breakfastbot/handlers/sign_on.clj
+++ src/breakfastbot/handlers/sign_on.clj
@@ -2,7 +2,8 @@
   (:require [breakfastbot.db :as db]
             [breakfastbot.db-ops :as db-ops]
             [breakfastbot.handlers.common :refer [answers person-date-matcher]]
-            [clojure.tools.logging :refer [info debug]]))
+            [clojure.tools.logging :refer [info debug]]
+            [java-time :as jt]))
 
 ;; very similar to sign-off
 (defn parse-sign-on
@@ -16,7 +17,7 @@
 (defn sign-on
   [who when]
   ;; ensure that we primed until this date...
-  (debug "performing sign-on for" who "on" when)
+  (debug "performing sign-on for" who "on" (jt/format when))
   (db-ops/prime-attendance db/db when)
   ;; is this a valid event at all?
   (if-not (:exists (db/any-attendance-on-date db/db {:day when}))
