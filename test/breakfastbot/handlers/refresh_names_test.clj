5480e4f2a69f11ca559cca57c284653d913347d8
studer-l <lukas.studer@alumni.ethz.ch>
2019-07-14 17:33:26 +0200

    delete old test

--- test/breakfastbot/handlers/refresh_names_test.clj
+++
@@ -1,6 +0,0 @@
-(ns breakfastbot.handlers.refresh-names-test
-  (:require [breakfastbot.handlers.refresh-names :as sut]
-            [clojure.test :as t]
-            [mount.core :as mount]
-            [breakfastbot.db :as db]
-            [breakfastbot.db-test :refer [prepare-mock-db mock-emails mock-names]]))
