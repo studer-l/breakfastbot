79e282bd96c3e7ced5ac4608e8238b1f57e89d08
Lukas Studer <lukas.studer@distran.ch>
2022-04-12 14:14:49 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
-(defproject breakfastbot "1.0"
+(defproject breakfastbot "1.1"
   :description "Breakfast organizer bot for Zulip"
   :url "https://github.com/studer-l/breakfastbot"
   :license {:name "BSD-3-Clause"
             :url  "https://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.3.610"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.async "1.5.648"]
                  [mount "0.1.16"]
                  [tolitius/mount-up "0.1.3"]
-                 [org.clojure/java.jdbc "0.7.11"]
-                 [org.postgresql/postgresql "42.2.18"]
-                 [com.layerware/hugsql "0.5.1"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [clojure.java-time "0.3.2"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.3.3"]
+                 [com.layerware/hugsql "0.5.3"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [clojure.java-time "0.3.3"]
                  [com.mchange/c3p0 "0.9.5.5"]
-                 [ch.qos.logback/logback-classic "1.2.3"]
-                 [migratus "1.3.2"]
+                 [ch.qos.logback/logback-classic "1.2.11"]
+                 [migratus "1.3.6"]
                  [org.clojars.studerl/clojure-zulip "0.4.5"]]
   :plugins [[lein-cloverage "1.1.1"]]
   :cloverage {:html? false}
