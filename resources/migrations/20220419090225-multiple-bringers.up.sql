0fe57d606b16b858eea7150622a614126b7eeb49
Lukas Studer <lukas.studer@distran.ch>
2022-04-22 14:12:59 +0200

    fixup migrations

--- resources/migrations/20220419090225-multiple-bringers.up.sql
+++ resources/migrations/20220419090225-multiple-bringers.up.sql
@@ -4,4 +4,6 @@ ALTER TABLE bringer DROP CONSTRAINT IF EXISTS day;
 --;;
 ALTER TABLE bringer DROP CONSTRAINT bringer_day_fkey;
 --;;
+ALTER TABLE bringer DROP CONSTRAINT IF EXISTS bringer_day_key;
+--;;
 ALTER TABLE bringer ADD FOREIGN KEY (day, id) REFERENCES attendances (day, id) ON DELETE CASCADE;
